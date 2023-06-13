ded2820e203bc02472ee4821dc04f58ff3fa1302
Anton Sobolev <antonsobolev1987@gmail.com>
2023-06-13 21:13:37 +0900

    Попытка исправить ошибки в проектах

--- Enter-in-CSharp/Theory/Lesson1/Task2/Program.cs
+++ Enter-in-CSharp/Theory/Lesson1/Task2/Program.cs
@@ -1,10 +1,17 @@
-﻿ Console.WriteLine("Пожалуйста представьтесь");
-        string userName = Console.ReadLine();
-        if (userName.ToLower() == "маша")
-        {
-            Console.WriteLine("Ура! Это же " + UserName + "!");
-        }
-        else
-        {
-            Console.WriteLine("Привет, " + UserName + "!");
-        }
+﻿Console.WriteLine("Пожалуйста представьтесь");
+string userName = Console.ReadLine();
+if (userName is not null)
+{
+    if (userName.ToLower() == "маша")
+    {
+        Console.WriteLine("Ура! Это же " + userName + "!");
+    }
+    else
+    {
+        Console.WriteLine("Привет, " + userName + "!");
+    }
+}
+else
+{
+    Console.WriteLine("Вы не ввели ни одного символа!!");
+}
