6178f298bcd46cb1cccbb3d19f23a0c516f938fc
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-15 17:05:43 +0900

    Сделал task3

--- Enter-in-CSharp/Practice/Lesson2/HomeWork/Task3/Program.cs
+++ Enter-in-CSharp/Practice/Lesson2/HomeWork/Task3/Program.cs
@@ -4,3 +4,21 @@
 7 -> да
 1 -> нет
 */
+
+Console.WriteLine("Введите номер дня недели");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+if (dayWeek == 6 || dayWeek == 7)
+{
+    Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это выходной!");
+}
+else
+if (dayWeek == 1 || dayWeek == 2 || dayWeek == 3 || dayWeek == 4 || dayWeek == 5)
+{
+    Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это будний день!");
+}
+else
+if(dayWeek == 0 || dayWeek > 7){
+    Console.WriteLine("Вы ввели число, несопоставимое ни с одним днем недели!");
+}
+
+
