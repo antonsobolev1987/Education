d1e92f73a90d20376151296ab4656ea7a5f45933
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-15 17:14:57 +0900

    task3 - добавил решение case

--- Enter-in-CSharp/Practice/Lesson2/HomeWork/Task3/Program.cs
+++ Enter-in-CSharp/Practice/Lesson2/HomeWork/Task3/Program.cs
@@ -7,6 +7,8 @@
 
 Console.WriteLine("Введите номер дня недели");
 int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+/* Решение №1
 if (dayWeek == 6 || dayWeek == 7)
 {
     Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это выходной!");
@@ -17,8 +19,44 @@ if (dayWeek == 1 || dayWeek == 2 || dayWeek == 3 || dayWeek == 4 || dayWeek == 5
     Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это будний день!");
 }
 else
-if(dayWeek == 0 || dayWeek > 7){
+if (dayWeek == 0 || dayWeek > 7)
+{
     Console.WriteLine("Вы ввели число, несопоставимое ни с одним днем недели!");
 }
+*/
+//Решение №2
+
+switch (dayWeek)
+{
+    //case 0:
+        //Console.WriteLine("Вы ввели число, несопоставимое ни с одним днем недели!");
+        //break;
+    case 1:
+        Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это будний день!");
+        break;
+    case 2:
+        Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это будний день!");
+        break;
+    case 3:
+        Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это будний день!");
+        break;
+    case 4:
+        Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это будний день!");
+        break;
+    case 5:
+        Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это будний день!");
+        break;
+    case 6:
+        Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это выходной!");
+        break;
+    case 7:
+        Console.WriteLine("Вы ввели " + dayWeek + " день недели. Это выходной!");
+        break;
+    default:
+        Console.WriteLine("Вы ввели число, несопоставимое ни с одним днем недели!");
+        break;
+}
+
+
 
 
