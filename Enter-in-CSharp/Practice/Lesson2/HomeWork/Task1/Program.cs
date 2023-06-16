dbd03ed3aff6f9475da278ef07f9ab33a6026aa9
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-16 11:39:29 +0900

    task1 сделал

--- Enter-in-CSharp/Practice/Lesson2/HomeWork/Task1/Program.cs
+++ Enter-in-CSharp/Practice/Lesson2/HomeWork/Task1/Program.cs
@@ -4,3 +4,20 @@
 782 -> 8
 918 -> 1
 */
+
+void userCount(int number1)
+{
+    number1 = System.Math.Abs(number1) % 100 / 10;
+    Console.WriteLine($"Вторая цифра этого числа: {number1}");
+}
+
+Console.WriteLine("Введите трёхзначное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 > 99 && number1 < 999 || number1 < -99 && number1 > -999)
+{
+    userCount(number1);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число!!");
+}
