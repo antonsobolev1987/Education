3336569b791806a22a5be5592d37734be6024437
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-16 14:47:12 +0900

    task3 сделана

--- Enter-in-CSharp/Practice/Lesson2/HomeWork/Task2/Program.cs
+++ Enter-in-CSharp/Practice/Lesson2/HomeWork/Task2/Program.cs
@@ -4,3 +4,20 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
+
+int inputNumber = Math.Abs(new Random().Next());
+int intermNumber = 0;
+Console.WriteLine($"Сгенерировано число {inputNumber}");
+if (inputNumber >= 100)
+{
+    while(inputNumber > 999){
+    inputNumber /= 10;
+    }
+    intermNumber = inputNumber % 10;
+    Console.WriteLine($"3 цифра числа: {intermNumber}");
+}
+
+else
+{
+    Console.WriteLine($"В сгенерированном числе нет 3ей цифры!!");
+}
