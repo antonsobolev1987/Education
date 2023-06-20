4320d36af84424735774a33326c3d942ff43a199
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-20 11:58:41 +0900

    Сделал Task3 Lesson3

--- Enter-in-CSharp/Practice/Lesson3/Task3/Program.cs
+++ Enter-in-CSharp/Practice/Lesson3/Task3/Program.cs
@@ -2,3 +2,48 @@
 //A(x1,y1); B(x2,y2)
 
 //distanceAB = квадр. корень из (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)
+
+Console.WriteLine("Введите координату X для точки А:");
+int coordXPointA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y для точки А:");
+int coordYPointA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X для точки B:");
+int coordXPointB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y для точки B:");
+int coordYPointB = Convert.ToInt32(Console.ReadLine());
+
+double distAB(int coordXPointA, int coordYPointA, int coordXPointB, int coordYPointB)
+{
+    double result = Math.Sqrt(coordXPointB - coordXPointA) * (coordXPointB - coordXPointA) + (coordYPointB - coordYPointA) * (coordYPointB - coordYPointA);
+    Console.WriteLine($"2D-расстояние между точками = {result:f2}");
+    return result;
+
+}
+
+void enterCoordPoint(int coordXPointA, int coordYPointA, int coordXPointB, int coordYPointB)
+{
+    while (coordXPointA == coordXPointB && coordXPointA == coordYPointA && coordXPointA == coordYPointB)
+    {
+        Console.WriteLine("Координаты точек не могут быть равными между собой! Введите заново");
+        Console.WriteLine("Введите координату X для точки А:");
+        coordXPointA = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координату Y для точки А:");
+        coordYPointA = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координату X для точки B:");
+        coordXPointB = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координату Y для точки B:");
+        coordYPointB = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("ОК");
+    distAB(coordXPointA, coordXPointB, coordYPointA, coordYPointB);
+}
+
+enterCoordPoint(coordXPointA, coordXPointB, coordYPointA, coordYPointB);
+
+
