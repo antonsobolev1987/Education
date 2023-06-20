bd2d3c88df424e3884986ee67299c558169c736e
Anton Sobolev <antonsobolev1987@gmail.com>
2023-06-20 20:47:45 +0900

    Добавил Lesson4

--- Enter-in-CSharp/Practice/Lesson4/Task1/Program.cs
+++ Enter-in-CSharp/Practice/Lesson4/Task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1.
+//Напишите программу, которая принимает на
+// вход число (А) и выдаёт сумму чисел от 1 до А.
+
+//int countA = Convert.toint32(math.abs(Console.readline());
+//Console.writeline(countA);
+void findsum1toA(int num_a)
+{
+    int sumOfElem = 0;
+    for (int i = 0; i <= num_a; i++)
+    {
+        sumOfElem += i;
+    }
+    Console.WriteLine(sumOfElem + " ");
+}
+
+Console.WriteLine("Введите число: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+
+if (user_num > 0){
+
+    findsum1toA(user_num);
+}
+else
+{
+    Console.WriteLine("Error!");
+}
--- Enter-in-CSharp/Practice/Lesson4/Task2/Program.cs
+++ Enter-in-CSharp/Practice/Lesson4/Task2/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Вычислить сумму чисел нацело делящихся на 5.
+//Цикл задать от 0 до введенного с клавиатуры числа
