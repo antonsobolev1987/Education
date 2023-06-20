ddaa109f792e90c753b799f1de4918cf9bec1693
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-20 16:26:26 +0900

    Сделал Task1 ДЗ

--- Enter-in-CSharp/Practice/Lesson3/Homework/Task1/Program.cs
+++ Enter-in-CSharp/Practice/Lesson3/Homework/Task1/Program.cs
@@ -4,3 +4,33 @@
 12821 -> да
 23432 -> да
 */
+
+Console.WriteLine("Введите число от 100: ");
+int symbols = Convert.ToInt32(Console.ReadLine());
+while (symbols < 100)
+{
+    Console.WriteLine("Слишком короткое число для проверки на палиндромность! Введите число от 100:");
+    symbols = Convert.ToInt32(Console.ReadLine());
+}
+checkPal();
+
+void checkPal()
+{
+    int tempsymbols = symbols;
+    string strtempsymbols = "";
+    while (tempsymbols > 0)
+    {
+        int ost = tempsymbols % 10;
+        tempsymbols = tempsymbols /= 10;
+        strtempsymbols = strtempsymbols + ost.ToString();
+    }
+
+    if (strtempsymbols == symbols.ToString())
+    {
+        Console.WriteLine("Ваше число палиндром");
+    }
+    else
+        Console.WriteLine("Ваше число не палиндром");
+}
+
+
