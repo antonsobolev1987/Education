e19a070c5b1785381cde32b15d563d2aca43578d
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-20 15:16:11 +0900

    Сделал task4 lesson3 и task3 HW

--- Enter-in-CSharp/Practice/Lesson3/Homework/Task3/Program.cs
+++ Enter-in-CSharp/Practice/Lesson3/Homework/Task3/Program.cs
@@ -3,3 +3,24 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+void raschetPOW(int userNumber)
+{
+    int count = 1;
+    Console.Write("Ряд кубов представлен как:");
+    while (count <= userNumber)
+    {
+        double result = Math.Pow(count, 3);
+        count++;
+        Console.Write($" {result}");
+}
+    }
+    
+Console.WriteLine("Введите число: ");
+int userNumber = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+while (userNumber == 0)
+{
+    Console.WriteLine("Невозможно возвести в куб 0. Введите корректное значение!");
+    userNumber = Convert.ToInt32(Console.ReadLine());
+}
+raschetPOW(userNumber);
--- Enter-in-CSharp/Practice/Lesson3/Task4/Program.cs
+++ Enter-in-CSharp/Practice/Lesson3/Task4/Program.cs
@@ -1,3 +1,33 @@
 ﻿//программа принимает на вход число N и выдает ряд квадратов чисел от 1 до N
 //5 -> 1 4 9 16 25
 //2 -> 1 4
+void raschetPOW(int userNumber)
+{
+    int count = 1;
+    Console.Write("Ряд квадратов представлен как:");
+    while (count <= userNumber)
+    {
+        double result = Math.Pow(count, 2);
+        count++;
+        Console.Write($" {result}");
+}
+    }
+    
+Console.WriteLine("Введите число: ");
+int userNumber = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+while (userNumber == 0)
+{
+    Console.WriteLine("Невозможно вычислить квадратный корень из нуля");
+    userNumber = Convert.ToInt32(Console.ReadLine());
+}
+raschetPOW(userNumber);
+
+
+
+
+
+
+
+
+
