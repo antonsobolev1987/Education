4ca32ef9e65b4c94e15d8034f281ea29f6fbc94c
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-22 16:38:33 +0900

    Сделал дз к 4 семинару

---
+++ Enter-in-CSharp/Practice/Lesson4/Homework/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber(int num)
+{
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = num - num % 10;
+        result = result + (num - advance);
+        num = num / 10;
+    }
+    return result;
+}
+
+int summa = sumNumber(num);
+Console.WriteLine("Сумма цифр в числе: " + summa);
