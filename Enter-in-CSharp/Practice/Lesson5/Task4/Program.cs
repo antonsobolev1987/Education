3d0108e8f608aec4e121b85a37ccb18cd3298165
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-26 17:18:41 +0900

    task4

---
+++ Enter-in-CSharp/Practice/Lesson5/Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задайте одномерный массив из 123 эл.
+// Найти кол-во эл. массива, значения которых лежат в отрезке 10-99
+
+
+
+
+int[] randomArray()
+{
+    int size = new Random().Next(4, 10);
+    int[] randomArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        randomArray[i] = new Random().Next(-10, 11);
+    }
+    return randomArray;
+}
+
+void printArray(int[] arrayPrint)
+{
+    for (int i = 0; i < arrayPrint.Length; i++)
+    {
+
+        Console.Write(arrayPrint[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void countOfIndex(int [] countArray){
+    int count =0;
+    for(int i =0; i< countArray.Length;i++){
+        if (countArray[i]>=10&&countArray[i]<=99)
+        count++;
+    }
+Console.WriteLine($"->{count}");
+}
+
+int [] randomArray = randomArray();
+printArray(randomArray);
+countOfIndex(randomArray);
