5c65e852ab47dc2bd3d0faf48bb7ab315982c4f3
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-27 16:46:09 +0900

    Task3

--- Enter-in-CSharp/Practice/Lesson5/Homework/Task3/Program.cs
+++ Enter-in-CSharp/Practice/Lesson5/Homework/Task3/Program.cs
@@ -3,3 +3,37 @@
 [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 */
 
+double[] array = new double[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(new Random().NextDouble(), 2) + new Random().Next(-100, 100);
+}
+
+Console.WriteLine("Задан массив:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+    if (i != array.Length - 1) Console.Write(" | ");
+}
+Console.WriteLine(" ");
+
+double minValue = 0;
+double maxValue = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < minValue)
+    {
+        minValue = array[i];
+
+    }
+    else if (array[i] > maxValue)
+    {
+        maxValue = array[i];
+    }
+}
+
+double sumEl = maxValue - minValue;
+Console.Write($"Разница между максимальным и минимальным элементом массива = {sumEl}");
+
