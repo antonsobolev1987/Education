b540804cc5683ac2174014c1c64e7d359156e166
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-27 15:48:06 +0900

    Сделал Task1

--- Enter-in-CSharp/Practice/Lesson5/Homework/Task1/Program.cs
+++ Enter-in-CSharp/Practice/Lesson5/Homework/Task1/Program.cs
@@ -3,3 +3,40 @@
 [345, 897, 568, 234] -> 2
 */
 
+int[] randomArray()
+{
+    int size = new Random().Next(1, 10);
+    int[] randomArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        randomArray[i] = new Random().Next(99, 1000);
+    }
+    return randomArray;
+}
+
+void printArray(int[] arrayPrint)
+{
+    for (int i = 0; i < arrayPrint.Length; i++)
+    {
+
+        Console.Write(arrayPrint[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+int [] array =  randomArray();
+printArray(array);
+
+int kolEl = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        kolEl++;
+    }
+    }
+Console.WriteLine($"Кол-во четных чисел в массиве = {kolEl}");
+
+
+
