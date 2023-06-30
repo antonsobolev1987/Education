6d8ec7646a00bc33367c482e3b3c21a8803b26d4
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-30 15:46:23 +0900

    Task4

---
+++ Enter-in-CSharp/Practice/Lesson6/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает на вход три числа и проверяет может ли существовать
+//треугольник с сторонами такой длины
+//каждая сторона треугольника меньше суммы двух других сторон
+//a<b+c
+//b<a+c
+//c<a+b
+
+void Triangle(int firstSide, int secondSite, int thirdSide)
+{
+    int[] sides = { firstSide, secondSite, thirdSide };
+    if (sides[0] < sides[1] + sides[2] && sides[1] < sides[0] + sides[2] && sides[2] < sides[0] + sides[1])
+    {
+        Console.WriteLine("Треугольник существует");
+
+    }
+    else Console.WriteLine("Треугольник не существует");
+}
+
+Triangle(3, 40, 500);
