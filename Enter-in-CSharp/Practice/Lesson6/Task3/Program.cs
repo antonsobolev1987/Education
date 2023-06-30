1a3ef7a1b7e70633ef9bcc39c4b0ece2bcd2a5f3
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-06-30 11:23:38 +0900

    Task3

---
+++ Enter-in-CSharp/Practice/Lesson6/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿//Не используя рекурсию вывести первые n чисел
+//последовательности Фибоначчи
+
+void printArray(int[] arrayToShow)
+{
+    for (int i = 0; i < arrayToShow.Length; i++)
+    {
+        Console.Write(arrayToShow[i]);
+        if (i != arrayToShow.Length - 1)
+        {
+            Console.Write(" | ");
+        }
+    }
+    Console.WriteLine();
+}
+
+void Fibonacci(int n, int a, int b)
+{
+    int[] arrayFibonacci = new int[n];
+    arrayFibonacci[0]  = a;
+    arrayFibonacci[1] = b;
+    for (int i = 2; i < n; i++)
+    {
+        arrayFibonacci[i] = arrayFibonacci[i - 1] + arrayFibonacci[i - 2];
+    }
+    printArray(arrayFibonacci);
+}
+
+Console.Write("Введите число элементов: ");
+int userCount = Convert.ToInt32(Console.ReadLine());
+
+if (userCount > 2)
+{
+    Console.WriteLine("Введите 1 значение: ");
+    int userFirstNum = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 2 значение: ");
+    int userSecondNum = Convert.ToInt32(Console.ReadLine());
+    Fibonacci(userCount, userFirstNum, userSecondNum);
+}
+else Console.WriteLine("Не верные значения");
