a0cb78de3237e1f999b05f9f7c678815fe6b7f79
Anton Sobolev <80943971+antonsobolev1987@users.noreply.github.com>
2023-07-10 11:25:19 +0900

    Task3

--- Enter-in-CSharp/Practice/Lesson8/Homework/Task3/Program.cs
+++ Enter-in-CSharp/Practice/Lesson8/Homework/Task3/Program.cs
@@ -8,3 +8,59 @@
 18 20
 15 18
 */
+Console.Clear();
+int[,] CreatedArray(int sizeArroy)
+{
+    int[,] createdArray = new int[sizeArroy, sizeArroy];
+    for (int i = 0; i < sizeArroy; i++)
+        for (int j = 0; j < sizeArroy; j++)
+            createdArray[i, j] = new Random().Next(1, 9);
+    return createdArray;
+}
+
+void ShowArray(int[,] printedarray)
+{
+    for (int i = 0; i < printedarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < printedarray.GetLength(1); j++)
+        {
+            Console.Write(printedarray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите размерность создаваемых массивов: ");
+int sizeArroy = Convert.ToInt32(Console.ReadLine());
+
+int[,] firstArr = CreatedArray(sizeArroy);
+Console.WriteLine("Сгенерирован 1-ый массив: ");
+ShowArray(firstArr);
+
+int[,] secondArr = CreatedArray(sizeArroy);
+Console.WriteLine("Сгенерирован 2-ый массив: ");
+ShowArray(secondArr);
+
+int[,] resultArr = new int[sizeArroy, sizeArroy];
+Console.WriteLine("---------------------------------------------");
+
+void MultiplyArray(int[,] firstArray, int[,] secondArray, int[,] resultArray)
+{
+    for (int i = 0; i < resultArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultArray.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstArray.GetLength(1); k++)
+            {
+                sum += firstArray[i, k] * secondArray[k, j];
+            }
+            resultArray[i, j] = sum;
+        }
+    }
+}
+
+Console.WriteLine("Произведение двух массивов представлено массивом: ");
+MultiplyArray(firstArr, secondArr, resultArr);
+ShowArray(resultArr);
--- Enter-in-CSharp/Practice/Lesson8/Homework/Task5/Program.cs
+++ Enter-in-CSharp/Practice/Lesson8/Homework/Task5/Program.cs
@@ -7,3 +7,42 @@
 10 09 08 07
 */
 
+Console.Clear();
+
+int sizeArray = 4;
+int[,] sqareArray = new int[sizeArray, sizeArray];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareArray.GetLength(0) * sqareArray.GetLength(1))
+{
+    sqareArray[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < sqareArray.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= sqareArray.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > sqareArray.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+WriteArray(sqareArray);
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0){
+                Console.Write($" {array[i, j].ToString().PadLeft(2, '0')}");
+                }
+            else Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
